49d20064bcaaa6544d3d8a89704ea8070259ba31
@CX330_x <2810288656@qq.com>
2022-09-26 19:38:47 +0800

    博客系统基于前后端分离

---
+++ src/main/java/Dao/Test.java
@@ -0,0 +1,25 @@
+package Dao;
+
+import enity.Blog;
+import enity.user;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+/**
+ * @title: Test
+ * @Author Xu
+ * @Date: 2022/9/26 21:33
+ * @Version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        UserDao userDao = new UserDao();
+        // 1. 测试一下新增用户
+        user user = new user();
+        user.setUsername("oasis");
+        user.setPassword("123456");
+        UserDao dao = new UserDao();
+        userDao.insert(user);
+    }
+}
